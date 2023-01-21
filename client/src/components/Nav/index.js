a66f1edf2e1f33b12de4a224459aef2a21a5dffc
mohammadreza memar <mohammadrezamemar@mohammarezasMBP.attlocal.net>
2023-01-20 19:46:59 -0600

    styled the nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,3 +1,11 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Link } from "react-router-dom";
+
+const StyledNavbar = styled.nav`
+  background-color: #333;
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem;
+`;
