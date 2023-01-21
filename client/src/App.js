eeeef3889e2d848ced4b96f008d2ff5cf1b4fb76
mohammadreza memar <mohammadrezamemar@mohammarezasMBP.attlocal.net>
2023-01-20 19:50:27 -0600

    add a nav bar to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Landing from './scenes/Landing';
 import Footer from './components/Footer';
+import Navbar from './components/Nav';
 
 function App() {
   return (
     <div className="App">
       <Landing />
       <Footer/>
+      <Navbar/>
     </div>
   );
 }
