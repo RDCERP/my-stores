015898c867c6b94db6cb88d9f49b85f1842ec6e4
mohammadreza memar <mohammadrezamemar@mohammarezasMBP.attlocal.net>
2023-01-20 19:34:48 -0600

    export the footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Landing from './scenes/Landing';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Landing />
+      <Footer/>
     </div>
   );
 }
--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,5 +1,10 @@
 import react from 'react';
 
 const Footer = () => {
-    
+    return (
+        <h1>Mohammad</h1>
+    )
+
 }
+
+export default Footer;
