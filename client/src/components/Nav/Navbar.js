a661d5ce6d6ae21e4e87ae03969f3c709464d863
mohammadreza memar <mohammadrezamemar@mohammarezasMBP.attlocal.net>
2023-01-24 11:49:44 -0600

    working on sing-up button

--- client/src/components/Nav/Navbar.js
+++ client/src/components/Nav/Navbar.js
@@ -32,7 +32,18 @@ function Navbar () {
             </Link>
             {dropdown && <Dropdown/>}
           </li>
+          <li className='nav-item'>
+            <Link to='/contact-us' className='nav-links' onClick={closeMobileMenu}>
+              Contact Us
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+              Sign Up
+            </Link>
+          </li>
         </ul>
+        <Button/>
       </nav>
     </div>
   );
