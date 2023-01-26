8980e02f12cbf60e7400926b613cd7f7ece7cf90
mrdekraker <30578794+mrdekraker@users.noreply.github.com>
2023-01-26 16:54:11 -0600

    saving middleware/auth

--- server/controllers/middleware/auth.js
+++ server/controllers/middleware/auth.js
@@ -4,7 +4,9 @@ export const verifyToken = async (req, res, next) => {
   try {
     let token = req.header('Authorization');
 
-    if (!token) { return res.status(403).send('Access denied. No token provided.'); }
+    if (!token) {
+      return res.status(403).send('Access denied. No token provided.');
+    }
 
     if (token.startsWith('Bearer ')) {
       token = token.slice(7, token.length).trimLeft();
